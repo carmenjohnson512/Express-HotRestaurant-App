c7fdb387c92c4da15e9865b9f31853c22b403477
Ariel Cuesta <arielcc88@gmail.com>
2020-07-06 21:02:32 -0500

    api and post routes added. Testted with postman.

--- app.js
+++ app.js
@@ -12,12 +12,63 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//sample data
+const tables = [ {
+    id: "trilogy1",
+    name: "trilogy",
+    email: "some@email.com",
+    phone: "00000000000000"
+} ];
+
+const waitlist = [{
+    id: "trilogy2",
+    name: "frthvvv",
+    email: "some1@email.com",
+    phone: "00000000000000"
+} ];
+
+// Routes
+// =============================================================
+// Displays all table info.
+app.get("/api/tables", function (req, res) {
+  return res.json(tables);
+});
+
+// Displays all table info.
+app.get("/api/waitlist", function (req, res) {
+  return res.json(waitlist);
+});
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "./templates/reservations.html"));
+});
 
 
+//post route
+app.post("/add/customer", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    let customer  = req.body;
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    customer.routeName = customer.name.replace(/\s+/g, "").toLowerCase();
+    console.log(customer);
+    makeRes(customer)
+    res.json(customer);
+  });
 
+function makeRes(customer){
+    let count= tables.length;
+    if (count <= 5){
+        tables.push(customer)
+    }
+    else {
+        waitList.push(customer)
+    }
+}
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
